8a966891a1034f6556d3a70da9cf3d4d7a0bdaef
oborogri <grigore.oboroceanu@gmail.com>
2017-01-02 15:14:52 +0000

    Login view added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,27 @@
+import {inject} from 'aurelia-framework';
+import TweetService from 'services/tweet-service';
+
+@inject(TweetService)
 export class App {
-  constructor() {
-    this.message = 'Hello World!';
+  email = 'homer@simpson.com';
+  password = 'secret';
+
+  loggedIn = false;
+  firstName = '';
+  lastName = '';
+
+  constructor(ts) {
+    this.tweetService = ts;
+  }
+
+  addUser() {
+    this.tweetService.addUser(this.firstName, this.lastName);
+    this.firstName = '';
+    this.lastName = '';
+  }
+
+  login() {
+    console.log(`Logging in with: ${this.email}`);
+    this.loggedIn = true;
   }
 }
